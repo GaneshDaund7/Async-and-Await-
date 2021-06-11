e16d1b401ed0b5dfc18f6efbfba1bcf35e0e26e3
Ganesh Daund <daundganesh496@gmail.com>
2021-06-11 10:49:43 +0530

    Async and Await in C#

---
+++ FindingNoofCharctersin File.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aync_and_Await_in_csharp
+{
+    class FindingNoofCharctersin_File
+    {
+        public static void Main()
+        {
+            var task = new Task(CallMethod);
+            task.Start();
+            task.Wait();
+            Console.WriteLine();
+        }
+        public static async void CallMethod()
+        {
+            string filePath = @"C:\Users\DjS\source\repos\Aync and Await in csharp\Aync and Await in csharp\abc.txt";
+            Task<int> task = ReadFile(filePath);
+            Console.WriteLine("Other work to do...");
+            Console.WriteLine("Other work to do...");
+            Console.WriteLine("Other work to do...");
+            int length = await task;
+            Console.WriteLine("Total No of Charcters in File is: " + length);
+
+
+        }
+
+        public static async Task<int> ReadFile(string filePath)
+        {
+            int len = 0;
+            Console.WriteLine("File Reading is started.......");
+            StreamReader sr = new StreamReader(filePath);
+            string s = await sr.ReadToEndAsync();
+            len = s.Length;
+            Console.WriteLine("File Reading is Ended.......");
+            return len;
+        }
+    }
+}
